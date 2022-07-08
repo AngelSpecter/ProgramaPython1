6cbe9d08594393f4769ecce90e01c1cb4ba63541
MIGUEL ANGEL <angelspectrum7@gmail.com>
2022-07-07 22:07:06 -0500

    FUNCIONES PRINCIPALES AGREGADAS

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("AQUI ESTA EL NUEVO COMMIT");
+def __Presentacion_Author__():
+    print("Miguel Angel Ayala");
+    print("INGENIERO EN COMPUTACION");
+    print("ICO 2022");
+
+def __Main_Menu__():
+    print(" *** BIENVENID@ AL SISTEMA DE FUNCIONES BASICAS EN PYTHON ***");
+    print(" \t1. CALCULADORA BASICA");
+    print(" \t2. LISTA DE ELEMENTOS");
+    print(" \t3. PROXIMAMENTE");
+
+def __CALCULADORA__():
+    print("SELECCIONA LA OPERACION A USAR");
+    print("1. SUMA\t2. RESTA\t3. MULTIPLICACION\t4. DIVISION");
+    print("TU ELECCION : ");
+    
